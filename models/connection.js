a7ce7b6889a921d0a32e0d4098e7b8b59bcbd967
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-01-17 18:14:56 -0300

    Initial commit

---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+let schema = null;
+
+async function connection() {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+}
+
+module.exports = connection;
