9bc9581ed5becadfcb618cea7795d68464851ce2
lucasquearis <lucasquearis@hotmail.com>
2022-01-21 18:30:57 -0300

    remove comentarios

--- src/views/helpers/helpers.js
+++ src/views/helpers/helpers.js
@@ -13,38 +13,7 @@ const setUpUserWebStorage = () => {
   sessionStorage.setItem('tokenNickname', makeid(16));
 };
 
-// const formNickName = document.getElementById('form-nickName');
-
-// const nickNameDisplay = document.createElement('h2');
-
-// const updateNickDB = (oldNick, newNick) => {
-//   socket.emit('updateUser', {
-//     oldNick,
-//     newNick,
-//   });
-// };
-
-// const setDisplayNick = () => {
-//   const nickName = sessionStorage.getItem('tokenNickname');
-
-//   nickNameDisplay.innerText = nickName;
-//   const nicknameInput = document.getElementById('nickname-input').value || nickName;
-
-//   sessionStorage.setItem('tokenNickname', nicknameInput);
-//   nickNameDisplay.innerText = nicknameInput;
-//   formNickName.appendChild(nickNameDisplay);
-// };
-
-// formNickName.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const oldNick = sessionStorage.getItem('tokenNickname');
-//   setDisplayNick();
-//   const newNick = sessionStorage.getItem('tokenNickname');
-//   updateNickDB(oldNick, newNick);
-// });
-
 setUpUserWebStorage();
-// setDisplayNick();
 
 window.onbeforeunload = () => {
   const socket = window.io();
